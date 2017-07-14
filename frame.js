0222b527b16a4aecf0873ab0835b3ac88763262b
e2dubba <svirgilgoodwin@gmail.com>
2017-07-14 09:03:16 -0500

    first attempts at frame scripts

--- frame.js
+++ frame.js
@@ -2,5 +2,18 @@ vimfx.listen('getURL', callback => {
     var url = content.location.href.toString();
     console.log(url);
     callback(url)
-})
+});
+
+vimfx.listen('authhelper', ({example}, callback) => {
+    console.log('initialized!')
+    var pageTitle = document.getElementByClassName('page-title');
+    console.log('hello?')
+    pageTitle = pageTitle.innerHTML;
+    var controlNum = document.getElementByClass('table-fieldgroup__column ')
+        console.log(controlNum);
+    callback(pageTitle)
+});
+
+
+
     
